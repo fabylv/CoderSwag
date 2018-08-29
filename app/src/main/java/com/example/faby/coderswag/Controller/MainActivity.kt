82448d3640fd87005aa8f73f59552925be40e34e
Faby <fabylv@gmail.com>
2018-08-29 11:40:23 -0700

    added list view and adapter

--- app/src/main/java/com/example/faby/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/faby/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.faby.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.faby.coderswag.Model.Category
 import com.example.faby.coderswag.R
+import com.example.faby.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
