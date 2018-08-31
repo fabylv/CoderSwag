30ba69ed7e9261393e933b202487260542007649
Faby <fabylv@gmail.com>
2018-08-31 11:49:37 -0700

    crated productactivity

--- app/src/main/java/com/example/faby/coderswag/Adapters/CategoryRecycleAdapter.kt
+++ app/src/main/java/com/example/faby/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -11,11 +11,11 @@ import com.example.faby.coderswag.Model.Category
 import com.example.faby.coderswag.R
 import kotlinx.android.synthetic.main.category_list_item.view.*
 
-class CategoryRecycleAdapter(val context: Context, val categories: List<Category>) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category>, val itemClick: (Category) -> Unit) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(parent?.context).inflate(R.layout.category_list_item, parent, false)
-        return Holder(view)
+        return Holder(view, itemClick)
     }
 
     override fun getItemCount(): Int {
@@ -27,7 +27,7 @@ class CategoryRecycleAdapter(val context: Context, val categories: List<Category
     }
 
 
-    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+    inner class Holder(itemView: View?, val itemClick: (Category) -> Unit) : RecyclerView.ViewHolder(itemView) {
 
         val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
         val categoryName = itemView?.findViewById<TextView>(R.id.categoryName)
@@ -36,6 +36,7 @@ class CategoryRecycleAdapter(val context: Context, val categories: List<Category
             val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
             categoryImage?.setImageResource(resourceId)
             categoryName?.text = category.title
+            itemView.setOnClickListener { itemClick(category) }
         }
 
     }
---
+++ app/src/main/java/com/example/faby/coderswag/Utilities/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.faby.coderswag.Utilities
+
+const val EXTRA_CATEGORY = "category"
