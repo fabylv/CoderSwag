251b153132e0a6f654777ed4a0b495438943843f
Faby <fabylv@gmail.com>
2018-08-30 17:13:27 -0700

    custom adapter

--- app/src/main/java/com/example/faby/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/faby/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.faby.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.faby.coderswag.Adapters.CategoryAdapter
 import com.example.faby.coderswag.Model.Category
 import com.example.faby.coderswag.R
 import com.example.faby.coderswag.Services.DataService
@@ -10,13 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
